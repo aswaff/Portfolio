0deb666efc9dc4558975756423628ad969fbc369
Avery Swafford <44035215+aswaff@users.noreply.github.com>
2021-01-01 13:12:35 -0500

    Updated year on footer.

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -5,7 +5,7 @@ import './footer.styles.css'
 export default function Footer() {
     return (
         <div className="footer-wrapper">
-            <span>© 2020 aswaff.dev. All rights reserved.</span>
+            <span>© 2021 aswaff.dev. All rights reserved.</span>
         </div>
     );
 
