71fd095c43623216df3556f2317c08084839c066
unknown <averyds17@gmail.com>
2022-02-20 19:33:40 -0500

    Added video-saver project to projects

--- src/components/projects/projectData.js
+++ src/components/projects/projectData.js
@@ -2,7 +2,26 @@
 
 const ProjectData = [
     {
-        "title": "ECommerce Store (Ongoing)",
+        "title": "Video Saver",
+        "image": "/projects/video-saver.png",
+        "description": `A very fun fullstack project I envisioned. It communicates with a API I wrote for this 
+                        project (https://github.com/aswaff/video-saverAPI). The app lets you save videos, with the ability to 
+                        set a category. The homepage has a TikTok style swipe for videos, the search page lets you search for 
+                        the videos based on category selected. Please give time for the demo site to load, the front and backend 
+                        are hosted on Heroku, you may have to refresh after initial page load.
+                        `,
+        "github_link": "https://github.com/aswaff/video-saver",
+        "live_link": "https://video-save.herokuapp.com/",
+        "tech_used": {
+            "library": "Svelte",
+            "tech": "Iframely API, Node/ExpressJS, MongoDB, Mongoose, SwiperJS",
+
+        },
+        
+    },
+
+    {
+        "title": "ECommerce Store",
         "image": "/projects/ecommerce-store.jpg",
         "description": `As part of the Zero-To-Mastery React course, we created this ecommerce project.
                         It utilizes Redux, and firebase. It was a great code a long project to at least get familiar 
